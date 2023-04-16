166b25de072201d457b857b1633a12cbef196996
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-16 17:24:37 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const addBtn = document.querySelector('.menu__bottons__add');
+const deleteAllBtn = document.getElementsByClassName('menu__bottons__delete');
+const saveBtn = document.querySelector('.panel__buttons__save');
+const cancelBtn = document.querySelector('.panel__buttons__cancel');
+const deleteBtn = document.querySelector('.notes__note__header__delete');
+
+const noteArea = document.querySelector('.notes');
+const notePanel = document.querySelector('.panel');
+const textArea = document.querySelector('#text');
+const category = document.querySelector('#category');
+const error = document.querySelector('.panel__error');
+
+const showPanel = () => {
+	notePanel.style.display = 'flex';
+    console.log("asda");
+};
+
+addBtn.addEventListener('click', showPanel);
